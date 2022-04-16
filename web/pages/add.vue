74c47f42ee4982e11b5bb3604f2382dbc410873c
Quentin Lagache <madewithyellow@gmail.com>
2022-04-16 14:26:20 +0800

    feat: create nuxt pages and UI to add/edit tasks

---
+++ web/pages/add.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="max-w-md mx-auto">
+    <div class="py-8 text-center">
+      <SvgIcon name="icons/plus-circle" class="inline-block w-10 h-10" aria-hidden="true" />
+      <h1 class="mt-6 text-3xl font-extrabold text-gray-900">
+        Add a task
+      </h1>
+    </div>
+    <TaskForm />
+  </div>
+</template>
