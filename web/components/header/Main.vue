8ffbc03c2a67767f1219a9787fb462ade045f005
Quentin Lagache <madewithyellow@gmail.com>
2022-04-16 14:53:05 +0800

    feat(header): add link to create a task in main header

--- web/components/header/Main.vue
+++ web/components/header/Main.vue
@@ -1,9 +1,12 @@
 <template>
   <header class="bg-indigo-600">
-    <div class="container mx-auto h-20 flex justify-between items-center text-white">
-      <NuxtLink :to="{ name: 'index' }" class="inline-flex items-center gap-x-5">
+    <div class="container flex items-center justify-between h-20 px-4 mx-auto text-white sm:px-0">
+      <NuxtLink :to="{ name: 'index' }" class="inline-flex items-center font-medium gap-x-5">
         <SvgIcon name="icons/logo" class="w-10 h-10" />
         Task editor
+      </NuxtLink>
+      <NuxtLink v-if="$nuxt.$route.name === 'index'" :to="{ name: 'add' }" class="px-4 py-2 font-medium text-indigo-600 bg-white rounded-md">
+        Add
       </NuxtLink>
     </div>
   </header>
